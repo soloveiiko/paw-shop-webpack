f1e36b7c9a5cd8ba5f4115a4964edf847fc75ece
soloveiiko <soloveymaria51@gmail.com>
2023-08-29 11:15:00 +0300

    added: mixins and variables

--- webpack.config.js
+++ webpack.config.js
@@ -4,55 +4,55 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    mode: 'development',
-    devtool: 'inline-source-map',
-    entry: './src/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].[hash].js',
-    },
-    devServer: {
-        port: 8080,
-        open: true,
-        compress: true,
-        liveReload: true,
-        watchFiles: ["./src/*.html, ./src/*.(s*)css"],
-        hot: true,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: '[name].[hash].css',
-        }),
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(css|scss)$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ]
-            },
-            {
-                test: /\.(jpe?g|png|svg)/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '[name].[hash:6].[ext]',
-                            outputPath: 'images/',
-                            publicPath: 'images/',
-                            emitFile: true,
-                            esModule: false
-                        }
-                    }
-                ]
-            },
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ]
-    }
+      },
+      {
+        test: /\.(jpe?g|png|svg)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash:6].[ext]',
+              outputPath: 'images/',
+              publicPath: 'images/',
+              emitFile: true,
+              esModule: false
+            }
+          }
+        ]
+      },
+    ]
+  },
+  devServer: {
+    port: 8080,
+    open: true,
+    compress: true,
+    liveReload: true,
+    watchFiles: ["./src/*.html", "./src/*.(s*)css"],
+    hot: true,
+  },
 };
