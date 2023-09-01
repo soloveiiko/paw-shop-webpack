c6621aed266f39f02248dc5cd10203800935737b
soloveiiko <soloveymaria51@gmail.com>
2023-09-01 17:10:25 +0300

    added: hovers

--- src/index.js
+++ src/index.js
@@ -1,1 +1,28 @@
 import './style/style.scss';
+
+const catImage = document.querySelector('.banner__image_cat');
+const dogImage = document.querySelector('.banner__image_dog');
+const catButton = document.querySelector('.cat-btn');
+const dogButton = document.querySelector('.dog-btn');
+
+function handleImageMouseOver(button) {
+  button.classList.add('hover');
+}
+
+function handleImageMouseOut(button) {
+  button.classList.remove('hover');
+}
+
+catImage.addEventListener('mouseover', () => {
+  handleImageMouseOver(catButton);
+});
+catImage.addEventListener('mouseout', () => {
+  handleImageMouseOut(catButton);
+});
+
+dogImage.addEventListener('mouseover', () => {
+  handleImageMouseOver(dogButton);
+});
+dogImage.addEventListener('mouseout', () => {
+  handleImageMouseOut(dogButton);
+});
